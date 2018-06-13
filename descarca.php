c69cf6d3a2d76f7eb6fd95e6df77f300504301a8
ancabalaita <ancaa55@gmail.com>
2018-06-14 00:38:35 +0300

    Update descarca.php

--- descarca.php
+++ descarca.php
@@ -3,7 +3,7 @@
 <div class="descarca">
 	<h1 style="text-align:center">Aici puteti descarca retetele in urmatoarele formate</h1>
 	<br>
-<button type="submit" class="button formatbtn" name="format"><a href="results.json">Format JSON </a></button>
+<button type="submit" class="button formatbtn" name="format"><a href="json.php">Format JSON </a></button>
 <button type="submit" class="button formatbtn" name="format"><a href="csv.php">Format CSV </a></button>
 <button type="submit" class="button formatbtn" name="format"><a href="xml.php">Format XML </a></button>
 
